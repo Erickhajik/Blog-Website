98dff26207bb79c546384f919bd6de8e9648cd34
Erickhajik <erickhajik65@gmail.com>
2022-07-28 11:26:44 -0700

    Create InitializeDatabase.php

---
+++ InitializeDatabase.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Initialize Database</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "comp440";
+$password = "pass1234";
+$database = "comp440";
+$makeConnection = new mysqli($servername,$username,$password,$database);
+$filename = 'university.sql';
+$op_data = '';
+$lines = file($filename);
+foreach ($lines as $line)
+{
+    if (substr($line, 0, 2) == '--' || $line == '')
+    {
+        continue;
+    }
+    $op_data .= $line;
+    if (substr(trim($line), -1, 1) == ';')
+    {
+        $makeConnection->query($op_data);
+        $op_data = '';
+    }
+}
+echo "<h2>Table Created Inside</h2>". $database;
+?>
+
+</body>
+</html>
+<?php
+$servername = "localhost";
+$username = "comp440";
+$password = "pass1234";
+$database = "comp440"; 
+$makeConnection = new mysqli($servername,$username,$password,$database);
+$filename = 'user.sql'; 
+$op_data = '';
+$lines = file($filename);
+foreach ($lines as $line)
+{
+    if (substr($line, 0, 2) == '--' || $line == '')
+    {
+        continue;
+    }
+    $op_data .= $line;
+    if (substr(trim($line), -1, 1) == ';')
+    {
+        $makeConnection->query($op_data);
+        $op_data = '';
+    }
+}
+echo "Table Created Inside " . $database . "";
+?>
+
